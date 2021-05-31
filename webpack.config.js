5e1d02c3b8d6c413a29f9f2bca28a3ac1dc7cc50
ssalanitri <sergiosalanitri@gmail.com>
2021-05-31 00:47:13 -0300

    First version in this fork

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,15 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    mode: 'production'
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
+    mode: 'development',
     compress: true,
     port: 9000
   }
 };
+
+//Webpack agrupa todas las dependencias y generar un version desplegable de la aplicacion.
